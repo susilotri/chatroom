388c122c88003d3365a44c7205416dc846f7eecd
trisusilo48 <tri.susilo@altama.co.id>
2025-03-22 10:28:24 +0700

    init project

---
+++ storage/framework/views/985320970d6c64f7a16b2947b04f991a.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChatRoom</title>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.js']); ?>
+</head>
+<body class="bg-gray-100">
+    <div id="app">
+        <Login></Login>
+    </div>
+</body>
+</html>
+<?php /**PATH C:\laragon\www\chatroom-backend\resources\views/welcome.blade.php ENDPATH**/ ?>
